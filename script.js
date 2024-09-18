377ab4e902c631cefb175631ec88eb511febaf4f
icaro <icaroarmando21@gmail.com>
2024-09-18 12:41:36 -0300

    add accordion faq

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ hamburger_icon.addEventListener('click',() => {
     nav_list.classList.toggle('open');
     hamburger_icon.classList.toggle('active');
 });
+
+const mainAccordion = document.querySelector('.accordion-main .accordion-header');
+
+mainAccordion.addEventListener('click', () => {
+    const mainBody = mainAccordion.nextElementSibling;
+    mainBody.classList.toggle('active'); // Abre/fecha o conteúdo do Accordion pai
+    
+    // Quando o pai for clicado, vamos permitir a interação com os filhos
+    const childAccordions = mainBody.querySelectorAll('.accordion-child .accordion-header');
+
+    childAccordions.forEach(childAccordion => {
+        childAccordion.addEventListener('click', (event) => {
+            event.stopPropagation(); // Previne que o clique afete o Accordion pai
+            const childBody = childAccordion.nextElementSibling;
+            childBody.classList.toggle('active'); // Abre/fecha o conteúdo do Accordion filho
+        });
+    });
+});
+
